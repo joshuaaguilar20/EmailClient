8cd4fcb5cd6fe08779ba2aae78a967e7128fd9be
Joshua-Aguilar <joshuaaguilar20@gmail.com>
2018-11-24 19:15:39 -0800

    dev

---
+++ config/dev.js
@@ -0,0 +1,11 @@
+module.exports = {
+    googleClientID: "875253937718-q166bnips1b1j3t3bskioas3loq69247.apps.googleusercontent.com",
+    googleClientSecret: "googleClientSecret",
+    mongoURI: 'mongodb://joshuaaguilar20:J0shua11@ds113122.mlab.com:13122/email-dev',
+    cookieKey: 'ireallylovecookiesandebbyandsnugs',
+    stripePublicKey: "pk_test_nNrAdQIeNUfjQpHKtYzalw8A",
+    stripeSecretKey: 'sk_test_Ss9ahj3Yv9WPq1uQ8yt9oIcU'
+};
+
+
+
