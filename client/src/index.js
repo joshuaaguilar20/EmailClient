b43caea81003a35fbfebd3556e3a8c0880bba81e
joshua <joshuaaguilar20@gmail.com>
2018-12-18 12:44:53 -0800

    proxy changes

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import reduxThunk from 'redux-thunk';
-
 import App from './components/App';
 import reducers from './reducers';
 
